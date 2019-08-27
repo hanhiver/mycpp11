2bc2e0740c6344ed10545345adc4fc0a2a4b8160
Dong Han <handongfr@gmail.com>
2019-08-27 10:28:42 +0800

    update

--- 11special/autoType/autoType.cpp
+++ 11special/autoType/autoType.cpp
@@ -3,11 +3,11 @@
 int main() 
 {
 	auto i = 42;
-	//std::vector<std::string> v; 
-	//v.append("Hello!");
+	float s[] = {1.0, 3.4, 5.9};
+	auto j = &s;
 
 	std::cout<<i<<std::endl;
-	//std::cout<<v[0]<<std::endl;
+	std::cout<<s[1]<<std::endl;
 
 	return 0;
 }
