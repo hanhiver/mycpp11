4d769090e8cba10c71dac57d25befcb4c07fda54
Dong Han <handongfr@gmail.com>
2019-09-01 21:43:10 +0800

    update

---
+++ container/unordered_set.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+	unordered_multiset<sting> cities {
+		"braunschweig", "Hanover", "frankfurt", "New York", 
+		"Chicago", "Toronto", "Paris", "frankfurt"
+	};
+
+	// print each element
+	for (const auto& elem : cities)
+	{
+		cout << elem << " ";
+	}
+	cout << endl; 
+
+	// insert additional values. 
+	cities.insert( {"London", "Munich", "Hanover", "Braunschweig"} );
+
+	// print each element.
+	for (const auto& elem : cities) 
+	{
+		cout << elem << " ";
+	}
+	cout << endl;
+}
