632ede47c436e62d056ad4a4e2350028c9b8de78
hanhiver <13910395157@139.com>
2019-09-04 17:45:40 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <vector>
+#include "common.hpp"
 using namespace std; 
 
 int main()
 {
 	vector<int> vect; 
+	vector<int> vect2 {1, 2, 3, 4, 5, 6};
 
 	vect.push_back(2);
 	vect.push_back(3);
 	vect.push_back(4);
 
+	print(vect);
+	vect.clear();
+	print(vect);
+	vect = std::move(vect2);
+	vect2.clear();
+	print(vect);
 	
 
 }
