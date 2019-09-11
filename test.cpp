98578684b118828d226d06766ba98255c2741224
hanhiver <13910395157@139.com>
2019-09-11 20:01:21 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include "common.hpp"
 using namespace std; 
+#define RH_ETCD_KEY_JOB                       RH_ETCD_KEY_REDHARE_VER_PRE "/job"
 
 int main()
 {
-	vector<int> vect; 
-	vector<int> vect2 {1, 2, 3, 4, 5, 6};
+	static string h = "hello";
 
-	vect.push_back(2);
-	vect.push_back(3);
-	vect.push_back(4);
-
-	print(vect);
-	vect.clear();
-	print(vect);
-	vect = std::move(vect2);
-	vect2.clear();
-	print(vect);
-	
-
+	cout << boolalpha << h.compare("hello") << endl;
+	cout << boolalpha << h.compare("hellO") << endl;
+	cout << RH_ETCD_KEY_JOB << endl;
 }
