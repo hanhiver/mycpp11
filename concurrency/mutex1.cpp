201b1072440e65f104e46c27adc95ffd47ec17ba
hanhiver <handongfr@163.com>
2019-09-14 15:47:06 +0800

    update

--- concurrency/mutex1.cpp
+++ concurrency/mutex1.cpp
@@ -46,4 +46,5 @@ int main()
     auto f4 = std::async(std::launch::async, print_lock, 
                          "Greeting from a second thread. ");
     print_lock("Greeting from the main thread. ");
+    
 }
