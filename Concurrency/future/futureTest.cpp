e7d502b0b1f606f0e52e1575afde597aeae95ed5
Dong Han <handongfr@gmail.com>
2019-09-22 23:52:28 +0800

    update

--- Concurrency/future/futureTest.cpp
+++ Concurrency/future/futureTest.cpp
@@ -9,13 +9,20 @@ int sayHello(std::string who)
     return 0;
 }
 
+void sayByebye()
+{
+    std::cout << "Byebye. " << std::endl;
+}
+
 int main()
 {
     std::future<int> s1 = std::async(sayHello, "Dady");
     std::future<int> s2 = std::async(sayHello, "Monmy");
+    std::future<void> s3 = std::async(sayByebye);
 
-    s1.get();
-    s2.get();
+    int a = s1.get();
+    int b = s2.get();
+    s3.get();
 
     return 0;
 }
