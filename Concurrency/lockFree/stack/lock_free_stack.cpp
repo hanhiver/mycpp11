fbef23bfec343f7ef01607cdf59efaf296f989f3
Dong Han <handongfr@gmail.com>
2019-10-03 14:07:46 +0800

    update

--- Concurrency/lockFree/stack/lock_free_stack.cpp
+++ Concurrency/lockFree/stack/lock_free_stack.cpp
@@ -20,6 +20,9 @@ public:
     {
         node* const new_node = new node(data);
         new_node->next = head.load();
+        // (1)这里用原子操作查看head是否等于new_node->next。
+        // 如果相等，则将new_node的值赋给head，函数返回true，循环退出。
+        // 如果不相等，表明可能其他线程修改了head的值，则将head的新值赋给new_node->next，函数返回false，回到(1)继续循环。
         while(!head.compare_exchange_weak(new_node->next, new_node));
     }
 };
