bdadc1c1b7f2c0c32263a40557ba9d6067c16404
Dong Han <handongfr@gmail.com>
2019-10-03 19:31:41 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
-#include "common.hpp"
+#include <thread> 
+#include <memory>
+#include <atomic> 
+
 using namespace std; 
 
 int main()
@@ -9,4 +12,12 @@ int main()
 
 	cout << boolalpha << h.compare("hello") << endl;
 	cout << boolalpha << h.compare("hellO") << endl;
+	cout << std::thread::id() << endl;
+
+	int a = 42; 
+	shared_ptr<int> pa = make_shared<int>(a);
+	cout << *pa << endl;
+	// 判断平台是否提供一个无锁实现的shared_ptr()
+	cout << boolalpha << atomic_is_lock_free(&pa) << endl;
+
 }
