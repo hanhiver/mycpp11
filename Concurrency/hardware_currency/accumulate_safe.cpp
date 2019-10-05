5517be00f8a38d9afd4b07fd7ceba48412cf312c
Dong Han <handongfr@gmail.com>
2019-10-05 08:38:37 +0800

    update

--- Concurrency/hardware_currency/accumulate_safe.cpp
+++ Concurrency/hardware_currency/accumulate_safe.cpp
@@ -16,6 +16,79 @@ struct accumulate_block
     }
 };
 
+class join_threads
+{
+private:
+    std::vector<std::thread> _threads; 
+
+public:
+    join_threads(std::vector<std::thread>& threads) : 
+        _threads(threads)
+    {
+    }
+
+    ~join_threads()
+    {
+        for (unsigned int i=0; i<_threads.size(); ++i)
+        {
+            if (_threads[i].joinable())
+            {
+                _threads[i].join();
+            }
+        }
+    }
+};
+
+// No try-catch version. 
+template<typename Iterator, typename T>
+T parallel_accumulate(Iterator first, Iterator last, T init)
+{
+    unsigned long const length = std::distance(first, last);
+
+    if (!length)
+    {
+        return init;
+    }
+
+    unsigned long const min_per_thread = 25;
+    unsigned long const max_threads = (length + min_per_thread -1)/min_per_thread;
+    unsigned long const hardware_threads = std::thread::hardware_concurrency();
+    unsigned long const num_threads = 
+                    std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
+    unsigned long const block_size = length / num_threads;
+    std::cout << "Using " << num_threads << " threads. " << std::endl; 
+
+    std::vector<std::future<T>> futures(num_threads - 1);
+    std::vector<std::thread> threads(num_threads - 1);
+    //join_threads joiner(threads);
+
+    Iterator block_start = first;
+    T result = init;
+
+    for (unsigned long i=0; i<(num_threads-1); ++i)
+    {
+        Iterator block_end = block_start;
+        std::advance(block_end, block_size);
+        std::packaged_task<T(Iterator, Iterator)> task(std::move(accumulate_block<Iterator, T>()));
+        //std::packaged_task<T(Iterator, Iterator)> task(accumulate_block<Iterator, T>());
+        futures[i] = task.get_future();
+        threads[i] = std::thread(std::move(task), block_start, block_end);
+        block_start = block_end;
+    }
+    //auto ab = accumulate_block<Iterator, T>();
+    //T last_result = ab(block_start, last);
+    T last_result = accumulate_block<Iterator, T>()(block_start, last);
+    
+    std::for_each(threads.begin(), threads.end(), 
+                std::mem_fn(&std::thread::join));
+
+    result += last_result;
+    
+    return result;
+}
+
+// Try-catch version. 
+/*
 template<typename Iterator, typename T>
 T parallel_accumulate(Iterator first, Iterator last, T init)
 {
@@ -79,6 +152,7 @@ T parallel_accumulate(Iterator first, Iterator last, T init)
     }
     return result;
 }
+*/
 
 int main()
 {
--- Concurrency/hardware_currency/accumulate_safe_async.cpp
+++ Concurrency/hardware_currency/accumulate_safe_async.cpp
@@ -11,16 +11,16 @@ template<typename Iterator, typename T>
 T parallel_accumulate(Iterator first, Iterator last, T init)
 {
     unsigned long const length = std::distance(first, last);
-    unsigned long const max_chunk_size = 25;
+    unsigned long const max_chunk_size = 100000;
 
-    if (length < max_chunk_size)
+    if (length <= max_chunk_size)
     {
-        return std::accumulate(first, last, T());
+        return std::accumulate(first, last, init);
     }
     else
     {
         Iterator mid_point = first; 
-        std::advance(first, length/2);
+        std::advance(mid_point, length/2);
         
         std::future<T> first_half_result = std::async(parallel_accumulate<Iterator, T>, first, mid_point, init);
         T second_half_result = parallel_accumulate<Iterator, T>(mid_point, last, T());
