806a740a854b9c3aaefd3982d9738af783544abf
hanhiver <handongfr@163.com>
2019-10-09 19:12:21 +0800

    update

---
+++ evpp/EventLoop/event_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include <thread> 
+#include <functional>
+#include <chrono>
+#include <evpp/event_loop.h>
+
+void hello()
+{
+    std::cout << "Hello from evpp/event_loop! " << std::endl; 
+}
+
+int main()
+{
+    evpp::EventLoop m_loop; 
+    m_loop.RunInLoop(hello);
+    m_loop.Run();
+    return 0;
+}
