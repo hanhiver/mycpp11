b2154e8837a4589fe661abe341bdac582cb63adb
hanhiver <handongfr@163.com>
2019-10-14 15:58:30 +0800

    update

---
+++ c++11/basic/new_delete/new_delete.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+int const LENGTH = 10;
+
+int main()
+{
+    unsigned long* array = new unsigned long[LENGTH];
+
+    for (int i=0; i<LENGTH; ++i)
+    {
+        array[i] = i*i; 
+    }
+
+    for (int i=0; i<LENGTH; ++i)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl; 
+
+    delete[] array;
+    return 0; 
+}
