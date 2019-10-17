f73e2d6819929b4f57774a3b8eec9e38972d02b3
hanhiver <handongfr@163.com>
2019-10-17 17:38:36 +0800

    update

--- evpp/EventLoop/event_loop.cpp
+++ evpp/EventLoop/event_loop.cpp
@@ -9,10 +9,13 @@ void hello()
     std::cout << "Hello from evpp/event_loop! " << std::endl; 
 }
 
+
 int main()
 {
     evpp::EventLoop m_loop; 
+    //m_loop.Run();
     m_loop.RunInLoop(hello);
     m_loop.Run();
+
     return 0;
 }
