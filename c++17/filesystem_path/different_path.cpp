9303a32b0aef99f097b4c87e072a9d80cb379f47
hanhiver <handongfr@163.com>
2019-10-31 22:36:53 +0800

    update

--- c++17/filesystem_path/different_path.cpp
+++ c++17/filesystem_path/different_path.cpp
@@ -2,9 +2,19 @@
 #include <filesystem> 
 
 using namespace std; 
-using namespace std::filesystem
+using namespace std::filesystem;
 
 int main()
 {
-    path p {""}
+    path p {"/home/dhan/myprog/c_cpp/mycpp11/test.cpp"}; 
+
+    cout << "full path       : " << p << '\n'; 
+    cout << "root_path()     : " << p.root_path() << '\n'
+         << "relative_path() : " << p.relative_path() << '\n'
+         << "parent_path()   : " << p.parent_path() << '\n'
+         << "filename()      : " << p.filename() << '\n'
+         << "stem()          : " << p.stem() << '\n'
+         << "extension()     : " << p.extension() << '\n'; 
+
+    return 0; 
 }
