3c7c89b46455609f15a212e3f50a4be05124e94e
hanhiver <handongfr@163.com>
2019-10-31 11:40:31 +0800

    update

--- c++17/mt19937/mt19937.cpp
+++ c++17/mt19937/mt19937.cpp
@@ -3,6 +3,7 @@
 #include <random> 
 #include <algorithm> 
 #include <execution> 
+#include <chrono>
 
 static bool odd(int n)
 {
@@ -20,19 +21,39 @@ int main()
             return dis(gen);
         });
 
-    
-    std::generate(std::execution::par, std::begin(d), std::end(d), rand_num); 
-    std::sort(std::execution::par, std::begin(d), std::end(d));
-    std::reverse(std::execution::par, std::begin(d), std::end(d));
-    auto odds(std::count_if(std::execution::par, std::begin(d), std::end(d), odd));
-    
-    /*
-    std::generate(std::begin(d), std::end(d), rand_num); 
-    std::sort(std::begin(d), std::end(d));
-    std::reverse(std::begin(d), std::end(d));
-    auto odds(std::count_if(std::begin(d), std::end(d), odd));
-    */
+    std::cout << "=== Serial Version: ===" << std::endl; 
+    {
+        auto start = std::chrono::steady_clock::now(); 
 
-    std::cout << (100.0 * odds / d.size()) 
+        std::generate(std::begin(d), std::end(d), rand_num); 
+        std::sort(std::begin(d), std::end(d));
+        std::reverse(std::begin(d), std::end(d));
+        auto odds(std::count_if(std::begin(d), std::end(d), odd));
+        
+        auto end = std::chrono::steady_clock::now();
+        auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+        double period = double(dur.count());
+
+        std::cout << "TIME: " << period << "ms, "
+                << (100.0 * odds / d.size()) 
                 << "% of the numbers are odd. " << std::endl; 
+    }
+
+    std::cout << "=== Parallel Version: ===" << std::endl; 
+    {
+        auto start = std::chrono::steady_clock::now(); 
+
+        std::generate(std::execution::par, std::begin(d), std::end(d), rand_num); 
+        std::sort(std::execution::par, std::begin(d), std::end(d));
+        std::reverse(std::execution::par, std::begin(d), std::end(d));
+        auto odds(std::count_if(std::execution::par, std::begin(d), std::end(d), odd));
+        
+        auto end = std::chrono::steady_clock::now();
+        auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
+        double period = double(dur.count());
+
+        std::cout << "TIME: " << period << "ms, "
+                << (100.0 * odds / d.size()) 
+                << "% of the numbers are odd. " << std::endl; 
+    }
 }
