b8bf449fc2eb73de62ad80169aea9a89614a22bc
hanhiver <handongfr@163.com>
2020-01-09 13:57:27 +0800

    update

--- prototype/engine/src/engine.cpp
+++ prototype/engine/src/engine.cpp
@@ -4,7 +4,7 @@
 #include <fstream> 
 #include <string>
 
-#include "../proto/config.pb.h"
+#include "config.pb.h"
 #include <google/protobuf/text_format.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
