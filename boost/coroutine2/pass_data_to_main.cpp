b4996832f2d7f19d33673d6db0cf663314b189c7
hanhiver <handongfr@163.com>
2020-01-23 00:36:58 +0800

    update

--- boost/coroutine2/pass_data_to_main.cpp
+++ boost/coroutine2/pass_data_to_main.cpp
@@ -1,9 +1,21 @@
 #include <iostream> 
 #include <boost/coroutine2/all.hpp>
 
+void foo(boost::coroutines2::coroutine<int>::push_type& sink)
+{
+    std::cout << " coroutine 1 " << std::endl; 
+    sink(1); 
+    std::cout << " coroutine 2 " << std::endl; 
+    sink(2); 
+    std::cout << " coroutine 3 " << std::endl; 
+    sink(3); 
+}
+
 int main()
 {
     typedef boost::coroutines2::coroutine<int> coro_t2; 
+    coro_t2::pull_type source(foo);
+    /*
     coro_t2::pull_type source(
         [&](coro_t2::push_type& sink) 
         {
@@ -14,7 +26,7 @@ int main()
             std::cout << " coroutine 3 " << std::endl; 
             sink(3); 
         });
-    
+    */
     while(source)
     {
         int ret = source.get(); 
