d80845fff66ef7a9dbb17df9e5b0b05507b83862
hanhiver <handongfr@163.com>
2020-05-17 18:36:47 +0800

    update

---
+++ dev_tools/gtest/first_example.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+int add(int a, int b)
+{
+    return a + b; 
+}
+
+TEST(testCase, test0)
+{
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(testCase, test1)
+{
+    EXPECT_GE(add(1, 2), 1);
+}
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
