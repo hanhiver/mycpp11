da5dde61ea413e5cf3751b59e0c6eca09aa8d3d9
hanhiver <handongfr@163.com>
2020-10-09 09:30:53 +0800

    update

--- prototype/SDK_online_manager/src/core/init.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DAMO_SDK_ONLINE_MANAGER_INIT_HPP
-#define DAMO_SDK_ONLINE_MANAGER_INIT_HPP
-
-#include <iostream> 
-
-int GlobalInit(int* pargc, char*** pargv);
-
-#endif  // DAMO_SDK_ONLINE_MANAGER_INIT_HPP
