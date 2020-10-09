c4361d32a1448dd0aff6668533873db14c8be553
hanhiver <handongfr@163.com>
2020-10-09 17:21:47 +0800

    update

--- mylibs/hide_private/src/timer.cpp
+++ mylibs/hide_private/src/timer.cpp
@@ -1,83 +1,33 @@
 #include "timer.hpp"
 
-Timer::Timer(): _expired(true), _try_to_expire(false)
-{}
+class Timer::TimerImpl
+{
+public:
+    std::string mName; 
+};
+
+Timer::Timer()
+{
+    impl = new TimerImpl; 
+    impl->mName = "no-name";
+}
 
 Timer::Timer(const Timer& timer)
 {
-    _expired = timer._expired.load();
-    _try_to_expire = timer._try_to_expire.load();
+    impl->mName = timer.impl->mName;
 }
 
 Timer::~Timer()
 {
-    stop_timer();
+    impl->mName = "";
 }
 
-void Timer::start_timer(int interval, std::function<void()> task)
+void Timer::set_name(std::string in)
 {
-    // 如果timer已经启动，避免二次启动。
-    if (_expired == false)
-    {
-        return; 
-    }
-
-    // 启动一个异步的计时器，生成一个新的线程并等待。
-    _expired = false;
-    std::thread([this, interval, task]()
-    {
-        while (!_try_to_expire)
-        {
-            // 线程睡眠interval时间然后执行task，直到计时器停止。
-            std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-            task();
-        }
-
-        {
-            // 计时器需要停止，更新条件变量，唤醒主线程。
-            std::lock_guard<std::mutex> locker(_mutex);
-            _expired = true;
-            _expired_cond.notify_one();
-        }
-    }).detach();
+    impl->mName = in; 
 }
 
-void Timer::start_once(int delay, std::function<void()> task)
+void Timer::hello(std::string in)
 {
-    std::thread([delay, task]()
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-        task();
-    }).detach();
+    std::cout << in << " say Goodbye to " << impl->mName << std::endl;
 }
-
-void Timer::stop_timer()
-{
-    // 已经停止就不要再次停止。
-    if (_expired)
-    {
-        return;
-    }
-
-    if (_try_to_expire)
-    {
-        return;
-    }
-
-    // 等待倒计时终止。
-    _try_to_expire = true;
-    {
-        std::unique_lock<std::mutex> locker(_mutex);
-        _expired_cond.wait(locker, [this]()
-        {
-            return _expired == true;
-        });
-
-        // 重置计时器状态。
-        if (_expired = true)
-        {
-            _try_to_expire = false;
-        }
-    }
-}
-
