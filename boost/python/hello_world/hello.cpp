ff5c7cd791e8813f1a23a54ac2d3a86df72de993
hanhiver <handongfr@163.com>
2020-10-19 17:18:26 +0800

    update

---
+++ boost/python/hello_world/hello.cpp
@@ -0,0 +1,22 @@
+/*-------------------      hello.cpp     -------------------*/ 
+#include <string>
+
+struct World 
+{ 
+    void set(std::string msg) { this->msg = msg; } 
+    std::string greet() { return msg; } 
+    std::string msg; 
+}; 
+
+
+#include <boost/python.hpp>
+using namespace boost::python; 
+
+BOOST_PYTHON_MODULE(hello) 
+{ 
+    class_<World>("World") 
+        .def("greet", &World::greet) 
+        .def("set", &World::set) 
+    ; 
+} 
+/*-----------------------------------------------------------*/ 
---
+++ boost/python/hello_world/wrapper.cpp
@@ -0,0 +1,9 @@
+#include <boost/python.hpp>
+#include "hello_world.hpp"
+
+BOOST_PYTHON_MODULE(hello_ext)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
+
