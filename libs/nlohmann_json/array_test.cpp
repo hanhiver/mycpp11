6f1ff007f623e320291ca8e0cfce1155b63464c5
hanhiver <handongfr@163.com>
2021-02-24 14:04:25 +0800

    update

--- libs/nlohmann_json/array_test.cpp
+++ libs/nlohmann_json/array_test.cpp
@@ -53,7 +53,14 @@ int main()
     std::string two = R"(["two"])";
     std::string one_two = R"(["one", "two"])";
 
-    std::string test = R"(
+    std::string test1 = R"(
+        {
+            "str" : "test_str",
+            "ary" : "array"
+        }
+    )";
+
+    std::string test2 = R"(
         {
             "str" : "test_str",
             "ary" : ["one", "two"]
@@ -63,7 +70,7 @@ int main()
     json j = "hello";
     try
     {
-        j = nlohmann::json::parse(test.c_str());
+        j = nlohmann::json::parse(test2.c_str());
     }
     catch(const std::exception& e)
     {
@@ -71,8 +78,45 @@ int main()
     }
     std::cout << j.dump(4) << std::endl; 
 
-    std::cout << j.at("str").size() << ", " << (j.at("str") == json::value_t::string) << std::endl; 
-    std::cout << j.at("ary").size() << ", " << (j.at("str") == json::value_t::array) << std::endl; 
+    for (const auto& item : j.items())
+    {
+        if (item.value().type() == json::value_t::array)
+        {
+            std::cout << "KEY: " << item.key() << std::endl;
+            for (const auto& cid : item.value())
+            {
+                std::cout  << "\tVALUE: " << cid << ", match one: " << (cid == "one") << std::endl; 
+            }
+        }
+        else if (item.value().type() == json::value_t::string)
+        {
+            std::cout << "KEY: " << item.key() << ", VALUE: " << item.value() << std::endl;  
+        }
+        else
+        {
+            std::cout << "WRONG type. " << std::endl; 
+        }
+    }
+
+    std::string multi_line = R"(
+    {
+        "str" : "test_str",
+        "ary" : "i have multi-line \\\\input, but I am still a \\nstring. "
+    }
+    )";
+
+    try
+    {
+        j = nlohmann::json::parse(multi_line.c_str());
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << j.dump(4) << std::endl; 
+
+    //std::cout << j.at("str").size() << ", " << (j.at("str") == json::value_t::string) << std::endl; 
+    //std::cout << j.at("ary").size() << ", " << (j.at("str") == json::value_t::array) << std::endl; 
     //std::cout << is_any_of(one_two, "one") << std::endl; 
     //std::cout << is_any_of(one, "one") << std::endl; 
 }
